5c54dfdd59cee45c994a1d5b064dd3d6a2ba9a6f
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-04-02 23:21:57 +0800

    Data extracting done

--- FeederScraper.py
+++ FeederScraper.py
@@ -1,6 +1,7 @@
 # yang diperlukan :
 # pip install beautifulsoup4
 # pip install requests
+import csv
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
@@ -67,9 +68,9 @@ class FeederScraper:
         # mencari link yang belum memiliki akm
         i = 0
         prodi_not_empty = 0
-        all_data = []
+        table_data = []
         for row in rows:
-            if i == 2:  # 0 dan 1 adalah header dari table
+            if i >= 2 and i <= 4:  # 0 dan 1 adalah header dari table
                 # Select a href ke 3 yang beiriskan link yang belum memiliki akm
                 links = row.findChildren(['a'])[2]
                 if links.text != "0":
@@ -90,41 +91,44 @@ class FeederScraper:
                     # lihat row akm
                     # print(rows_akm)
 
-                    # METODE STACKOVERFLOW
-                    tableMatrix = []
-                    # Here you can do whatever you want with the data! You can findAll table row headers, etc...
-                    list_of_rows = []
-                    jumlah_row = 0
-                    row_start = 0
-                    for row in my_table_akm.findAll('tr')[1:]:
-                        list_of_cells = []
-                        if row_start>=1 :
-                            for cell in row.findAll('td'):
-                                text = cell.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ','')
-                                list_of_cells.append(text)
-                            list_of_rows.append(list_of_cells)
-                            jumlah_row += 1
-                        row_start += 1
-                    tableMatrix.append((list_of_rows, list_of_cells))
-                    print(tableMatrix)
-                    print(jumlah_row)
+                    # # Method SO
+                    # # Mengolah data agar disave ke excel
+                    # tableMatrix = []
+                    # # Here you can do whatever you want with the data! You can findAll table row headers, etc...
+                    # list_of_rows = []
+                    # jumlah_row = 0
+                    # row_start = 0
+                    # for row in my_table_akm.findAll('tr')[1:]:
+                    #     if row_start >= 1 :
+                    #         list_of_cells = []
+                    #         for cell in row.findAll('td'):
+                    #             text = cell.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ','')
+                    #             list_of_cells.append(text)
+                    #         list_of_rows.append(list_of_cells)
+                    #         jumlah_row += 1
+                    #     row_start += 1
+                    # tableMatrix.append((list_of_rows, list_of_cells))
+                    # # print(tableMatrix)
+                    # # print(type(tableMatrix))
+                    # # print(jumlah_row)
 
                     # METODE AZMI
-                    # # mengambil semua column dari masing-masing row
-                    # r = 0
-                    # for row_akm in rows_akm:
-                    #     row_data = []
-                    #     if r >= 2:
-                    #         columns_akm = row_akm.findChildren(['td'])
-                    #         # mengambil satu column dari semua column
-                    #         for column_akm in columns_akm:
-                    #             # print(column_akm.text.replace('			', '').replace(' 		', ''))
-                    #             row_data.append(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
-                    #     print(row_data)
-                    #     r += 1
-
+                    # mengambil semua column dari masing-masing row
+                    r = 0
+                    for row_akm in rows_akm:
+                        if r >= 2:
+                            columns_akm = row_akm.findChildren(['td'])
+                            # mengambil satu column dari semua column
+                            row_data = []
+                            for column_akm in columns_akm:
+                                # print(column_akm.text.replace('			', '').replace(' 		', ''))
+                                row_data.append(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
+                            table_data.append(row_data)
+                        r += 1
                     prodi_not_empty += 1
             i += 1
+        print(table_data)
+
         # jumlah data yang tidak kosong
         # print(prodi_not_empty)
 
