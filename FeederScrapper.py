afd194bd90edf89affe543c89e30344dccc2bdc5
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-04-03 21:11:09 +0800

    Added file : findfak.py (untuk mencari prodi dan fakultas yang sesuai)

--- FeederScrapper.py
+++ FeederScrapper.py
@@ -1,10 +1,3 @@
-# yang diperlukan :
-# pip install beautifulsoup4
-# pip install requests
-# pip install requests
-# pip install tqdm
-#pip install argparse
-
 # untuk excel
 import csv
 # untuk cprint
@@ -45,163 +38,165 @@ class FeederScraper:
         # get(url, headers -- berfungsi untuk menghindari deteksi bahwa kita lagi scrapping)
         try :
             result = session.post(self.url, headers=self.header,data=self.payload)
+            if result.status_code != 200:
+                report_error.append('Error ' + str(result.status_code) + ' - ' + links.attrs['href']);
+            else:
+                # untuk memastikan bahwa url accessible dengan keterangan
+                # 200 OK
+                # 401 Unauthorized
+                # 403 Forbidden
+                # 407 Proxy Auth Required
+                # maka :
+                # print(result.status_code)
+                # print(result.headers)
+                # print(result.text)
+                # exit()
 
-            # untuk memastikan bahwa url accessible dengan keterangan
-            # 200 OK
-            # 401 Unauthorized
-            # 403 Forbidden
-            # 407 Proxy Auth Required
-            # maka :
-            # print(result.status_code)
-            # print(result.headers)
-            # print(result.text)
-            # exit()
+                # mendapatkan informasi header
+                # print(result.headers)
 
-            # mendapatkan informasi header
-            # print(result.headers)
+                # menyimpan konten website
+                src = result.content
+                # print(src)
+                # exit()
 
-            # menyimpan konten website
-            src = result.content
-            # print(src)
-            # exit()
+                # objek BeautifulSoup
+                soup = BeautifulSoup(src, features="html.parser")
 
-            # objek BeautifulSoup
-            soup = BeautifulSoup(src, features="html.parser")
+                # mencari semua tag yang diinginkan
+                # links = soup.find_all("img")
+                table = soup.findChildren('table', {"class": "table table-striped table-condensed"})
 
-            # mencari semua tag yang diinginkan
-            # links = soup.find_all("img")
-            table = soup.findChildren('table', {"class": "table table-striped table-condensed"})
+                # This will get the first (and only) table. Your page may have more.
+                my_table = table[0]
 
-            # This will get the first (and only) table. Your page may have more.
-            my_table = table[0]
+                # You can find children with multiple tags by passing a list of strings
+                # rows = my_table.findChildren(['th', 'tr'])
+                rows = my_table.findChildren(['tr'])
 
-            # You can find children with multiple tags by passing a list of strings
-            # rows = my_table.findChildren(['th', 'tr'])
-            rows = my_table.findChildren(['tr'])
+                # tes hasil pencarian
+                # print(rows)
+                # print(rows[2])
+                # exit()
 
-            # tes hasil pencarian
-            # print(rows)
-            # print(rows[2])
-            # exit()
+                # initiate csv
+                nama_file = 'extract.csv'
+                file = open(nama_file,'w', encoding='utf-8',newline='')
+                writer = csv.writer(file,delimiter=',')
+                writer.writerow(['No', 'NIM', 'Nama Mahasiswa', 'Periode Masuk',' Status Keluar', 'Tanggal Keluar', 'Periode Lulus/DO', 'Status Aktivitas Kuliah Mahasiswa Per Semester (AKM)'])
 
-            # initiate csv
-            nama_file = 'extract.csv'
-            file = open(nama_file,'w', encoding='utf-8',newline='')
-            writer = csv.writer(file,delimiter=',')
-            writer.writerow(['No', 'NIM', 'Nama Mahasiswa', 'Periode Masuk',' Status Keluar', 'Tanggal Keluar', 'Periode Lulus/DO', 'Status Aktivitas Kuliah Mahasiswa Per Semester (AKM)'])
+                # mencari link yang belum memiliki akm
+                i = 0
+                prodi_not_empty = 0
+                table_data = []
+                jumlah_data = 0
+                report=[]
+                report_error=[]
+                for row in tqdm(rows):
+                    # if i == 44 : # 0 dan 1 adalah header dari table
+                    if i >= 2 : # 0 dan 1 adalah header dari table
 
-            # mencari link yang belum memiliki akm
-            i = 0
-            prodi_not_empty = 0
-            table_data = []
-            jumlah_data = 0
-            report=[]
-            report_error=[]
-            for row in tqdm(rows):
-                # if i == 44 : # 0 dan 1 adalah header dari table
-                if i >= 2 : # 0 dan 1 adalah header dari table
+                        jumlah_baris = 0
 
-                    jumlah_baris = 0
+                        # Output Nama Prodi
+                        # Select a href ke 3 yang beiriskan link yang belum memiliki akm
+                        links = row.findChildren(['a'])[2]
+                        # url akm
+                        # print(links.attrs['href'])
+                        # print(links.text)
 
-                    # Output Nama Prodi
-                    # Select a href ke 3 yang beiriskan link yang belum memiliki akm
-                    links = row.findChildren(['a'])[2]
-                    # url akm
-                    # print(links.attrs['href'])
-                    # print(links.text)
-
-                    # membuka url akm
-                    url_akm = links.attrs['href'];
-                    result_akm = session.post(url_akm, headers=self.header)
-                    if result_akm.status_code != 200 :
-                        report_error.append('Error ' + str(result_akm.status_code) + ' - ' + links.attrs['href']);
-                    else :
-                        # Lihat hasil
-                        # print(result_akm.text)
-                        src_akm = result_akm.content
-                        soup_akm = BeautifulSoup(src_akm, features="html.parser")
-                        table_akm = soup_akm.findChildren('table', {"class": "content"})
-                        my_table_akm = table_akm[0]
-                        rows_akm = my_table_akm.findChildren(['tr'])
-                        # lihat row akm
-                        # print(rows_akm)
-
-                        # METODE AZMI
-                        # mengambil semua column dari masing-masing row
-                        r = 0
-                        for row_akm in rows_akm:
-                            if r >= 2:
-                                columns_akm = row_akm.findChildren(['td'])
-                                # mengambil satu column dari semua column
-                                row_data = []
-                                for column_akm in columns_akm:
-                                    # print(column_akm.text.replace('			', '').replace(' 		', ''))
-                                    # print(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
-                                    # filewriter.writerow([column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ','')])
-                                    if column_akm.text=='' :
-                                        row_data.append(' ')
-                                    else :
-                                        row_data.append(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
-                                writer.writerow(row_data)
-
-                                # each individual row_data
-                                # print(row_data)
-
-                                table_data.append(row_data)
-
-                                jumlah_baris += 1
-                            r += 1
-                        prodi_not_empty += 1
-                        jumlah_data=jumlah_baris+jumlah_data
-                        if jumlah_baris == 0 :
-                            color = 'red'
+                        # membuka url akm
+                        url_akm = links.attrs['href'];
+                        result_akm = session.post(url_akm, headers=self.header)
+                        if result_akm.status_code != 200 :
+                            report_error.append('Error ' + str(result_akm.status_code) + ' - ' + links.attrs['href']);
                         else :
-                            color = 'blue'
-                        report_row = []
-                        report_row.append(row.findChildren(['td'])[1].text)
-                        report_row.append("Jumlah Baris : " + str(jumlah_baris))
-                        report.append(report_row)
-                        # cprint(row.findChildren(['td'])[1].text,'white')
-                        # cprint("Jumlah Baris : " + str(jumlah_baris) + '\n', color)
-                i += 1
-            # print(report)
+                            # Lihat hasil
+                            # print(result_akm.text)
+                            src_akm = result_akm.content
+                            soup_akm = BeautifulSoup(src_akm, features="html.parser")
+                            table_akm = soup_akm.findChildren('table', {"class": "content"})
+                            my_table_akm = table_akm[0]
+                            rows_akm = my_table_akm.findChildren(['tr'])
+                            # lihat row akm
+                            # print(rows_akm)
 
-            file.close()
+                            # METODE AZMI
+                            # mengambil semua column dari masing-masing row
+                            r = 0
+                            for row_akm in rows_akm:
+                                if r >= 2:
+                                    columns_akm = row_akm.findChildren(['td'])
+                                    # mengambil satu column dari semua column
+                                    row_data = []
+                                    for column_akm in columns_akm:
+                                        # print(column_akm.text.replace('			', '').replace(' 		', ''))
+                                        # print(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
+                                        # filewriter.writerow([column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ','')])
+                                        if column_akm.text=='' :
+                                            row_data.append(' ')
+                                        else :
+                                            row_data.append(column_akm.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('         ',''))
+                                    writer.writerow(row_data)
 
-            # reporting
-            if self.report=="on":
+                                    # each individual row_data
+                                    # print(row_data)
+
+                                    table_data.append(row_data)
+
+                                    jumlah_baris += 1
+                                r += 1
+                            prodi_not_empty += 1
+                            jumlah_data=jumlah_baris+jumlah_data
+                            if jumlah_baris == 0 :
+                                color = 'red'
+                            else :
+                                color = 'blue'
+                            report_row = []
+                            report_row.append(row.findChildren(['td'])[1].text)
+                            report_row.append("Jumlah Baris : " + str(jumlah_baris))
+                            report.append(report_row)
+                            # cprint(row.findChildren(['td'])[1].text,'white')
+                            # cprint("Jumlah Baris : " + str(jumlah_baris) + '\n', color)
+                    i += 1
+                # print(report)
+
+                file.close()
+
+                # reporting
+                if self.report=="on":
+                    print('\n')
+                    for rep in report:
+                        print(rep[0])
+                        print(rep[1])
+
+                # Error Reporting to error.txt
+                f = open("error.txt", "w+", encoding='utf-8')
+                jumlah_error = 0
+                for re in report_error:
+                    f.write(re +" \r\n")
+                    jumlah_error+=1
+                f.close()
+
+                # Summary
                 print('\n')
-                for rep in report:
-                    print(rep[0])
-                    print(rep[1])
+                print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
+                print(' [REPORT]  ', end='')
+                print('Jumlah Error : ' + str(jumlah_error) + ' (untuk detail bisa dilihat di-file error.txt)')
+                print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
+                print(' [INFO]  ', end='')
+                print('File csv telah disimpan dengan nama : ' + nama_file)
+                print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
+                print(' [SELESAI]  ',end='')
+                print('Total Baris : '+str(jumlah_data))
 
-            # Error Reporting to error.txt
-            f = open("error.txt", "w+", encoding='utf-8')
-            jumlah_error = 0
-            for re in report_error:
-                f.write(re +" \r\n")
-                jumlah_error+=1
-            f.close()
+                # semua data
+                # print(table_data)
 
-            # Summary
-            print('\n')
-            print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
-            print(' [REPORT]  ', end='')
-            print('Jumlah Error : ' + str(jumlah_error) + ' (untuk detail bisa dilihat di-file error.txt)')
-            print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
-            print(' [INFO]  ', end='')
-            print('File csv telah disimpan dengan nama : ' + nama_file)
-            print('[' + datetime.now().strftime("%H:%M:%S") + ']',end='')
-            print(' [SELESAI]  ',end='')
-            print('Total Baris : '+str(jumlah_data))
+                # close file excel
 
-            # semua data
-            # print(table_data)
-
-            # close file excel
-
-            # jumlah data yang tidak kosong
-            # print(prodi_not_em pty)
+                # jumlah data yang tidak kosong
+                # print(prodi_not_em pty)
         except session.exceptions.HTTPError as err:
             pass
 
@@ -215,21 +210,21 @@ print('\n')
 print('[*] dimulai @ '+date.today().strftime("/%d-%m-%Y/") + ' ' + datetime.now().strftime("%H:%M:%S"))
 print('\n')
 
-
+# membuat args
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
-parser.add_argument("--cookie", default='su4htlotdbupebr7c0gopi1p92', help="ini adalah cookie dari aplikasi feeder (PHPSESSID) dengan format : 'COOKIEVALUE' (tanpa petik)")
+parser.add_argument("--cookie", default='mclqfmd8o9idsfd6cea5b9ou67', help="ini adalah cookie dari aplikasi feeder (PHPSESSID) dengan format : 'COOKIEVALUE' (tanpa petik)")
 parser.add_argument("--semester", default='20182', help="ini adalah form_data dari aplikasi feeder saat berada dihalaman 'http://private-feeder.ulm.ac.id/home', form_data bisa dilihat melalui Inspect Element, semester dengan contoh format : '20182' (tanpa petik)")
 parser.add_argument("--report", default='on', help="ini adalah option untuk menampilkan report jumlah baris per-prodi, format : 'on' atau 'off' (tanpa petik)")
 
-
+# parsing args
 args = parser.parse_args()
+
 cookie=args.cookie
 print('Cookie yang digunakan : ' + cookie)
 semester=args.semester
 print('Semester yang dipilih : ' + semester)
 report=args.report
 print('Report : ' + report)
-
 
 print('\n')
 
@@ -246,12 +241,6 @@ header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image
 payload = {'jenis_chart': 'jumlahmhsnonakm',
            'id_smt': semester}
 
+# calling main function
 d = FeederScraper(url, header, payload, report)
 d.main()
-
-
-
-
-
-
-
