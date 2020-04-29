d3e917b6df966000e2c80060b4a7349937e4033f
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-04-29 14:53:23 +0800

    FIx

---
+++ konfigurasi_ini.py
@@ -0,0 +1,11 @@
+# Cookie
+default_cookie = "gj67cdvn303q51l11is4eccrl0"
+
+# Semester
+default_semester = "20182"
+
+# Report
+default_report = "on"
+
+# User agent
+default_useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
